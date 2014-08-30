1a8b6dd7109d92f19b63e14cadf5856fd17aa3e3
javel555 <javel555@gmail.com>
2014-08-30 19:01:50 +0900

    体験版動作環境

---
+++ master/lua/RandomEvent.lua
@@ -0,0 +1,102 @@
+function EV1(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+function EV2(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+
+function EV3(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+
+function EV4(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+
+function EV4(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+function EV5(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+function EV6(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+function EV7(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+function EV8(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
+
+function EV9(tel)
+--	base = tel:getRequest("reqBase")
+--	bai = tel:getRequest("reqBai")
+--	level = tel:getRequest("level")
+--
+--	val = base * level + bai * (level - 1)
+--
+--	tel:setResponce("result", val.."")
+end
